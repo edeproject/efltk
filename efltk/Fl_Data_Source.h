57b7030ab0febbf6d0f38a4cbebf3caaf7e4659a
Alexey Parshin <parshin@users.sf.net>
2002-10-24 16:15:01 +0000

    *** empty log message ***

--- efltk/Fl_Data_Source.h
+++ efltk/Fl_Data_Source.h
@@ -3,8 +3,15 @@
 #define __FL_DATA_SOURCE_H__
 
 class Fl_Data_Source {
+   Fl_Group *m_parent;
 public:
-   Fl_Data_Source() {};
+   // ctor
+   Fl_Data_Source(Fl_Group *) {};
+   
+   // load data into widgets
+   bool load();
+   // unload data from widgets
+   bool save();
 };
 
 #endif
