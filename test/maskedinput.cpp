9e0d7c9dc42d36d1990699a2a325234c6732a654
Alexey Parshin <parshin@users.sf.net>
2002-11-10 19:40:27 +0000

    Test for Fl_Masked_Input

---
+++ test/maskedinput.cpp
@@ -0,0 +1,52 @@
+//
+//
+// Demo program from the fltk documentation.
+//
+// Copyright 1998-1999 by Bill Spitzak and others.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems to "fltk-bugs@easysw.com".
+//
+
+#include <efltk/Fl.h>
+#include <efltk/Fl_Window.h>
+#include <efltk/Fl_Masked_Input.h>
+
+int main(int argc, char **argv) {
+    Fl_Window *window = new Fl_Window(300,180);
+
+    Fl_Masked_Input *d = new Fl_Masked_Input(100,20,150,24,"date (YYYY-MM-DD):");
+    d->mask("2999\\-19\\-39");
+    
+    Fl_Masked_Input *t1 = new Fl_Masked_Input(100,50,150,24,"time (HH:MM:SS):");
+    t1->mask("29\\:59\\:59");
+    
+    Fl_Masked_Input *t2 = new Fl_Masked_Input(100,80,150,24,"time (HH:MM AM/PM):");
+    t2->mask("29\\:59T\\M");
+    
+    Fl_Masked_Input *n = new Fl_Masked_Input(100,110,150,24,"name (Xxxxxxxxxxxxx):");
+    n->mask("Lzzzzzzzzzzz");
+    
+    window->end();
+    window->show(argc, argv);
+
+    return Fl::run();
+}
+
+//
+// End of "$Id$".
+//
