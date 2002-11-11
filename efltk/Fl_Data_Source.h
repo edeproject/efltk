006cb3db087bd3c95df808ad0bf2e58b0758a051
Alexey Parshin <parshin@users.sf.net>
2002-11-11 17:27:03 +0000

    Updated Alexey Parshin' e-mail address in copyright :)

--- efltk/Fl_Data_Source.h
+++ efltk/Fl_Data_Source.h
@@ -1,4 +1,20 @@
-// Fl_Data_Source.h by Alexey Parshin
+/***************************************************************************
+                          Fl_Data_Source.h  -  description
+                             -------------------
+    begin                : Fri Mar 31 2000
+    copyright            : (C) 2000-2002 by Alexey S.Parshin
+    email                : alexeyp@m7.tts-sf.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef __FL_DATA_SOURCE_H__
 #define __FL_DATA_SOURCE_H__
 
--- src/core/Fl_Data_Source.cpp
+++ src/core/Fl_Data_Source.cpp
@@ -1,4 +1,20 @@
-// Fl_Data_Source.cpp by Alexey Parshin, 2002-10-26
+/***************************************************************************
+                          Fl_Data_Source.cpp  -  description
+                             -------------------
+    begin                : Oct 31 2002
+    copyright            : (C) 2000-2002 by Alexey S.Parshin
+    email                : alexeyp@m7.tts-sf.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 
 #include <efltk/Fl_Data_Source.h>
 #include <efltk/Fl_Widget.h>
