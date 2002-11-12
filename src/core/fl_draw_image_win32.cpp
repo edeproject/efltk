b736257f9e1761147f9bbf5bb2f32a846d488be0
Mikko Lahteenmaki <laza2000@users.sf.net>
2002-11-12 21:22:51 +0000

    WIN32 update

--- src/core/fl_draw_image_win32.cpp
+++ src/core/fl_draw_image_win32.cpp
@@ -18,83 +18,95 @@ Fl_PixelFormat sys_fmt;
 HINSTANCE *sys_display = 0;
 BITMAPINFO *bmi;
 
-Fl_PixelFormat *Fl_Renderer::system_format() { return &sys_fmt; }
-
+Fl_PixelFormat *Fl_Renderer::system_format() {
+    Fl_Renderer::system_init();
+    return &sys_fmt;
+}
 bool Fl_Renderer::system_inited() {
     return _system_inited;
+}
+bool Fl_Renderer::big_endian() {
+    Fl_Renderer::system_init();
+    return false;
+}
+bool Fl_Renderer::lil_endian() {
+    Fl_Renderer::system_init();
+    return true;
 }
 
 void Fl_Renderer::system_init()
 {
-	/* Determine the screen depth */
-	int bitspp = SussScreenDepth();
+    if(_system_inited) return;
 
-	uint32 Rmask=0, Gmask=0, Bmask=0, Amask=0;
-	switch(bitspp) {
-		case 15:
-			Rmask = 0x00007c00;
-			Gmask = 0x000003e0;
-			Bmask = 0x0000001f;
-			bitspp = 16;
-			break;
-		case 16:
-			Rmask = 0x0000f800;
-			Gmask = 0x000007e0;
-			Bmask = 0x0000001f;
-			break;
-		case 24:
-		case 32:
-			/* GDI defined as 8-8-8 */
-			Rmask = 0x00ff0000;
-			Gmask = 0x0000ff00;
-			Bmask = 0x000000ff;
-			break;
-		default:
-			break;
-	}
+    /* Determine the screen depth */
+    int bitspp = SussScreenDepth();
 
-	sys_fmt.init(bitspp,Rmask,Gmask,Bmask,Amask);
+    uint32 Rmask=0, Gmask=0, Bmask=0, Amask=0;
+    switch(bitspp) {
+    case 15:
+        Rmask = 0x00007c00;
+        Gmask = 0x000003e0;
+        Bmask = 0x0000001f;
+        bitspp = 16;
+        break;
+    case 16:
+        Rmask = 0x0000f800;
+        Gmask = 0x000007e0;
+        Bmask = 0x0000001f;
+        break;
+    case 24:
+    case 32:
+        /* GDI defined as 8-8-8 */
+        Rmask = 0x00ff0000;
+        Gmask = 0x0000ff00;
+        Bmask = 0x000000ff;
+        break;
+    default:
+        break;
+    }
 
-	bool is16bit = (sys_fmt.bytespp == 2);
+    sys_fmt.init(bitspp,Rmask,Gmask,Bmask,Amask);
 
-	/* Suss out the bitmap info header */
-	int bmi_size = sizeof(*bmi);
-	if(is16bit) {
-		/* is16bit modes, palette area used for rgb bitmasks */
-		bmi_size += 3*sizeof(DWORD);
-	} 
+    bool is16bit = (sys_fmt.bytespp == 2);
+
+    /* Suss out the bitmap info header */
+    int bmi_size = sizeof(*bmi);
+    if(is16bit) {
+        /* is16bit modes, palette area used for rgb bitmasks */
+        bmi_size += 3*sizeof(DWORD);
+    }
 #if USE_COLORMAP
-	else if(sys_fmt.palette) {
-		bmi_size += sys_fmt.palette->ncolors * sizeof(RGBQUAD);
-	}
+    else if(sys_fmt.palette) {
+        bmi_size += sys_fmt.palette->ncolors * sizeof(RGBQUAD);
+    }
 #endif
-	bmi = (BITMAPINFO *)malloc(bmi_size);
-	
-	bmi->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-	bmi->bmiHeader.biPlanes = 1;
-	bmi->bmiHeader.biXPelsPerMeter = 0;
-	bmi->bmiHeader.biYPelsPerMeter = 0;
-	bmi->bmiHeader.biClrUsed = 0;
-	bmi->bmiHeader.biClrImportant = 0;
-	bmi->bmiHeader.biBitCount = sys_fmt.bitspp;
+    bmi = (BITMAPINFO *)malloc(bmi_size);
 
-	if(is16bit) {
-		// BI_BITFIELDS tells CreateDIBSection about the rgb masks in the palette
-		bmi->bmiHeader.biCompression = BI_BITFIELDS;
-		((uint32*)bmi->bmiColors)[0] = sys_fmt.Rmask;
-		((uint32*)bmi->bmiColors)[1] = sys_fmt.Gmask;
-		((uint32*)bmi->bmiColors)[2] = sys_fmt.Bmask;
-	} else {
-		bmi->bmiHeader.biCompression = BI_RGB;	// BI_BITFIELDS is for 565 vs 555
+    bmi->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi->bmiHeader.biPlanes = 1;
+    bmi->bmiHeader.biXPelsPerMeter = 0;
+    bmi->bmiHeader.biYPelsPerMeter = 0;
+    bmi->bmiHeader.biClrUsed = 0;
+    bmi->bmiHeader.biClrImportant = 0;
+    bmi->bmiHeader.biBitCount = sys_fmt.bitspp;
+
+    if(is16bit) {
+        // BI_BITFIELDS tells CreateDIBSection about the rgb masks in the palette
+        bmi->bmiHeader.biCompression = BI_BITFIELDS;
+        ((uint32*)bmi->bmiColors)[0] = sys_fmt.Rmask;
+        ((uint32*)bmi->bmiColors)[1] = sys_fmt.Gmask;
+        ((uint32*)bmi->bmiColors)[2] = sys_fmt.Bmask;
+    } else {
+        bmi->bmiHeader.biCompression = BI_RGB;	// BI_BITFIELDS is for 565 vs 555
 #if USE_COLORMAP
-                if(sys_fmt.palette) {
-                    extern void copy_palette(Fl_Colormap *map);
-                    copy_palette(sys_fmt.palette);
-                    for (int i=0; i < sys_fmt.palette->ncolors; i++) *((short*)(bmi->bmiColors)+i) = i;
-                }
+        if(sys_fmt.palette) {
+            extern void copy_palette(Fl_Colormap *map);
+            copy_palette(sys_fmt.palette);
+            for (int i=0; i < sys_fmt.palette->ncolors; i++) *((short*)(bmi->bmiColors)+i) = i;
+        }
 #endif
-	}
-	_system_inited = true;
+    }
+    _system_inited = true;
 }
 
 /*
@@ -165,86 +177,84 @@ static int SussScreenDepth()
 }
 
 bool Fl_Renderer::render_to_pixmap(uint8 *src, Fl_Rect *src_rect, Fl_PixelFormat *src_fmt, int src_pitch,
-									Pixmap dst, Fl_Rect *dst_rect, GC dst_gc, int flags)
+                                   Pixmap dst, Fl_Rect *dst_rect, GC dst_gc, int flags)
 {
-    if(!Fl_Renderer::system_inited())
-        return false;
+    Fl_Renderer::system_init();
 
     if(flags&FL_ALIGN_SCALE && src_rect->w()==dst_rect->w() && src_rect->h()==dst_rect->h()) {
         flags = 0;
     }
 
-	int X = dst_rect->x();
-	int Y = dst_rect->y();
-	int W = dst_rect->w();
-	int H = dst_rect->h();
-	int cx = src_rect->x();
-	int cy = src_rect->y();
-	bool ret = false;
+    int X = dst_rect->x();
+    int Y = dst_rect->y();
+    int W = dst_rect->w();
+    int H = dst_rect->h();
+    int cx = src_rect->x();
+    int cy = src_rect->y();
+    bool ret = false;
 
-	if(flags & FL_ALIGN_SCALE)
-	{
-		int dst_pitch = Fl_Renderer::calc_pitch(sys_fmt.bytespp, W);
-		bmi->bmiHeader.biWidth = W;  
-		bmi->bmiHeader.biHeight = -H; //Set to negative, cause in windoze bitmaps are upsidedown...
-		bmi->bmiHeader.biSizeImage = H * dst_pitch;
-		
-		uint8 *dst_ptr = new uint8[bmi->bmiHeader.biSizeImage];
-		if(Fl_Renderer::stretch(src, sys_fmt.bytespp, src_pitch, src_rect,
-			dst_ptr, sys_fmt.bytespp, dst_pitch, dst_rect))
-		{			
-			SetDIBitsToDevice(dst_gc, 
-				X, Y, W, H, //dst points - upper-left
-				0, 0, //src points - lower-left
-				0, H, //Scanline start/end
-				(LPSTR)(dst_ptr), //array
-				bmi, //bmi
-				(fl_palette ? DIB_PAL_COLORS : DIB_RGB_COLORS)
-				);
-			ret = true;
-		}
-		delete []dst_ptr;
-	} 
-	else
-	{
-		W = src_rect->w();
-		H = src_rect->h();
-		bmi->bmiHeader.biWidth = W;  
-		bmi->bmiHeader.biHeight = -H; //Set to negative, cause in windoze bitmaps are upsidedown...
-		bmi->bmiHeader.biSizeImage = H * src_pitch;
-		
-		//printf("%d %d %d %d\n", X, Y, W, H);	
-		uint8 *src_ptr=src;
-		SetDIBitsToDevice(dst_gc, 
-			X, Y, W, H, //dst points - upper-left
-			0, 0, //src points - lower-left
-			0, H, //Scanline start/end
-			(LPSTR)(src_ptr), //array
-			bmi, //bmi
-			(fl_palette ? DIB_PAL_COLORS : DIB_RGB_COLORS)
-			);
-		ret = true;
-	}
+    if(flags & FL_ALIGN_SCALE)
+    {
+        int dst_pitch = Fl_Renderer::calc_pitch(sys_fmt.bytespp, W);
+        bmi->bmiHeader.biWidth = W;
+        bmi->bmiHeader.biHeight = -H; //Set to negative, cause in windoze bitmaps are upsidedown...
+        bmi->bmiHeader.biSizeImage = H * dst_pitch;
 
-	return ret;
+        uint8 *dst_ptr = new uint8[bmi->bmiHeader.biSizeImage];
+        if(Fl_Renderer::stretch(src, sys_fmt.bytespp, src_pitch, src_rect,
+                                dst_ptr, sys_fmt.bytespp, dst_pitch, dst_rect))
+        {
+            SetDIBitsToDevice(dst_gc,
+                              X, Y, W, H, //dst points - upper-left
+                              0, 0, //src points - lower-left
+                              0, H, //Scanline start/end
+                              (LPSTR)(dst_ptr), //array
+                              bmi, //bmi
+                              (fl_palette ? DIB_PAL_COLORS : DIB_RGB_COLORS)
+                             );
+            ret = true;
+        }
+        delete []dst_ptr;
+    }
+    else
+    {
+        W = src_rect->w();
+        H = src_rect->h();
+        bmi->bmiHeader.biWidth = W;
+        bmi->bmiHeader.biHeight = -H; //Set to negative, cause in windoze bitmaps are upsidedown...
+        bmi->bmiHeader.biSizeImage = H * src_pitch;
+
+        //printf("%d %d %d %d\n", X, Y, W, H);
+        uint8 *src_ptr=src;
+        SetDIBitsToDevice(dst_gc,
+                          X, Y, W, H, //dst points - upper-left
+                          0, 0, //src points - lower-left
+                          0, H, //Scanline start/end
+                          (LPSTR)(src_ptr), //array
+                          bmi, //bmi
+                          (fl_palette ? DIB_PAL_COLORS : DIB_RGB_COLORS)
+                         );
+        ret = true;
+    }
+
+    return ret;
 }
 
 //make device context from pixmap and palette
-static HDC make_DC(HDC dc, HBITMAP pixmap, HPALETTE pal) 
+static HDC make_DC(HDC dc, HBITMAP pixmap, HPALETTE pal)
 {
-  HDC new_gc = CreateCompatibleDC(dc);
-  SetTextAlign(new_gc, TA_BASELINE|TA_LEFT);
-  SetBkMode(new_gc, TRANSPARENT);
+    HDC new_gc = CreateCompatibleDC(dc);
+    SetTextAlign(new_gc, TA_BASELINE|TA_LEFT);
+    SetBkMode(new_gc, TRANSPARENT);
 
-  if(pal) SelectPalette(new_gc, pal, FALSE);
+    if(pal) SelectPalette(new_gc, pal, FALSE);
 
-  SelectObject(new_gc, pixmap);	
-  return new_gc;
+    SelectObject(new_gc, pixmap);
+    return new_gc;
 }
 
-
 void Fl_Image::to_screen(int XP, int YP, int WP, int HP, int, int)
-{	
+{
     int X,Y,W,H;
     fl_clip_box(XP, YP, WP, HP, X, Y, W, H);
 
@@ -266,83 +276,65 @@ void Fl_Image::to_screen(int XP, int YP, int WP, int HP, int, int)
     // convert to Xlib coordinates:
     fl_transform(X,Y);
 
-	if(mask) {			
-		if(id) {		
-			// both color and mask:
+    if(mask) {
+        if(id) {
+            // both color and mask:
 #if 0
-	      HDC new_gc = make_DC((Pixmap)id, fl_palette);
-      
-			if(_masktype == ALPHA)
-			{
-				TransparentBlt(
-					fl_gc,
-					X,
-					Y,
-					W,
-					H,
-				
-					new_gc,
-					cx,
-					cy,
-					W,
-					H,
-				
-					0
-				);
-			} else if(_masktype == COLOR_KEY)
-			{
-				TransparentBlt(
-					fl_gc,
-					X,
-					Y,
-					W,
-					H,
-				
-					new_gc,
-					cx,
-					cy,
-					W,
-					H,
-				
-					fmt.colorkey
-				);
-			} else {
-				BitBlt(fl_gc, X, Y, W, H, new_gc, cx, cy, SRCCOPY);
-			}
+            HDC new_gc = make_DC((Pixmap)id, fl_palette);
 
-			DeleteDC(new_gc);
+            if(_masktype == ALPHA)
+            {
+                TransparentBlt(
+                               fl_gc,
+                               X, Y, W, H,
+                               new_gc,
+                               cx, cy, W, H,
+                               0 );
+            } else if(_masktype == COLOR_KEY)
+            {
+                TransparentBlt(
+                               fl_gc,
+                               X, Y, W, H,
+                               new_gc,
+                               cx, cy, W, H,
+                               fmt.colorkey );
+            } else {
+                BitBlt(fl_gc, X, Y, W, H, new_gc, cx, cy, SRCCOPY);
+            }
+
+            DeleteDC(new_gc);
 # else
-      fl_color(0);
-      SetTextColor(fl_gc, 0);
-      HDC new_gc = make_DC(fl_gc, (Pixmap)mask, fl_palette);
-	  HDC new_gc2= make_DC(fl_gc, (Pixmap)id, fl_palette);
+            fl_color(0);
+            SetTextColor(fl_gc, 0);
+            HDC new_gc = make_DC(fl_gc, (Pixmap)mask, fl_palette);
+            HDC new_gc2= make_DC(fl_gc, (Pixmap)id, fl_palette);
 
-	  BitBlt(new_gc2, 0, 0, w, h, new_gc, 0, 0, SRCAND); // This should be done only once for performance
-      // secret bitblt code found in old MSWindows reference manual:
-      BitBlt(fl_gc, X, Y, W, H, new_gc, cx, cy, 0xE20746L);
-      BitBlt(fl_gc, X, Y, W, H, new_gc2, cx, cy, SRCPAINT);
-      DeleteDC(new_gc);
-      DeleteDC(new_gc2);		
+            BitBlt(new_gc2, 0, 0, w, h, new_gc, 0, 0, SRCAND); // This should be done only once for performance
+            // secret bitblt code found in old MSWindows reference manual:
+            BitBlt(fl_gc, X, Y, W, H, new_gc, cx, cy, 0xE20746L);
+            BitBlt(fl_gc, X, Y, W, H, new_gc2, cx, cy, SRCPAINT);
+            DeleteDC(new_gc);
+            DeleteDC(new_gc2);
 # endif
-    } else {
-      // mask only
-      HDC tempdc = CreateCompatibleDC(fl_gc);
-      SelectObject(tempdc, (HGDIOBJ)mask);
-      SetTextColor(fl_gc, 0); // VP : seems necessary at least under win95
-      //SelectObject(fl_gc, fl_brush);
-      // secret bitblt code found in old MSWindows reference manual:
-      BitBlt(fl_gc, X, Y, W, H, tempdc, cx, cy, 0xE20746L);
-      DeleteDC(tempdc);	  
+        } else {
+            // mask only
+            HDC tempdc = CreateCompatibleDC(fl_gc);
+            SelectObject(tempdc, (HGDIOBJ)mask);
+            SetTextColor(fl_gc, 0); // VP : seems necessary at least under win95
+            //SelectObject(fl_gc, fl_brush);
+            // secret bitblt code found in old MSWindows reference manual:
+            BitBlt(fl_gc, X, Y, W, H, tempdc, cx, cy, 0xE20746L);
+            DeleteDC(tempdc);
+        }
     }
-  }
-	else if (id) {		
-		// pix only, no mask
-		//fl_copy_offscreen(X, Y, W, H, (Pixmap)id, cx, cy);
-		HDC new_gc = make_DC(fl_gc, (Pixmap)id, fl_palette);		
-		BitBlt(fl_gc, X, Y, W, H, new_gc, cx, cy, SRCCOPY);				
-		DeleteDC(new_gc);
-		
-  } // else { no mask or id, probably an error... }
+    else if (id) {
+        // pix only, no mask
+        //fl_copy_offscreen(X, Y, W, H, (Pixmap)id, cx, cy);
+        HDC new_gc = make_DC(fl_gc, (Pixmap)id, fl_palette);
+        BitBlt(fl_gc, X, Y, W, H, new_gc, cx, cy, SRCCOPY);
+        DeleteDC(new_gc);
+
+    } // else { no mask or id, probably an error... }
 }
 
 void Fl_Image::to_screen_tiled(int XP, int YP, int WP, int HP, int, int)
@@ -355,22 +347,19 @@ void Fl_Image::to_screen_tiled(int XP, int YP, int WP, int HP, int, int)
     cx += X-XP; cy += Y-YP;
     fl_push_clip(X, Y, W, H);
 
-    {
-        int temp = -cx % w;
-        cx = (temp>0 ? w : 0) - temp;
-        temp = -cy % h;
-        cy = (temp>0 ? h : 0) - temp;
+    int temp = -cx % w;
+    cx = (temp>0 ? w : 0) - temp;
+    temp = -cy % h;
+    cy = (temp>0 ? h : 0) - temp;
 
-        int ccx=cx;
-        while (-cy < H) {
-            while (-cx < W) {
-                to_screen(X-cx, Y-cy, w, h, 0,0);
-                cx -= w;
-            }
-            cy -= h;
-            cx = ccx;
+    int ccx=cx;
+    while (-cy < H) {
+        while (-cx < W) {
+            to_screen(X-cx, Y-cy, w, h, 0,0);
+            cx -= w;
         }
-
+        cy -= h;
+        cx = ccx;
     }
 
     fl_pop_clip();
@@ -378,36 +367,38 @@ void Fl_Image::to_screen_tiled(int XP, int YP, int WP, int HP, int, int)
 
 uint8 *Fl_Renderer::data_from_pixmap(Pixmap src, Fl_Rect &rect, int &bitspp)
 {
+    Fl_Renderer::system_init();
+
     int x = rect.x();
     int y = rect.y();
     int w = rect.w();
     int h = rect.h();
     int	width, height, clipx, clipy;
     //int src_x, src_y; //NYI
-	int src_w, src_h;
-	LPBITMAPINFOHEADER dib_hdr;
-	int dib_size;
+    int src_w, src_h;
+    LPBITMAPINFOHEADER dib_hdr;
+    int dib_size;
 
-	dib_size = sizeof(BITMAPINFOHEADER) + 256 * sizeof (RGBQUAD);
-	dib_hdr = (LPBITMAPINFOHEADER)malloc(dib_size);
-	memset(dib_hdr, 0, dib_size);
-	dib_hdr->biSize = sizeof(BITMAPINFOHEADER);	
-	if( !GetDIBits(fl_gc, src, 0, rect.h(), NULL, (LPBITMAPINFO)dib_hdr, DIB_RGB_COLORS) )
-	{
-		free(dib_hdr);
-		//printf("GetDIBits(1) FAILED\n");		
-		return 0;
-	}
+    dib_size = sizeof(BITMAPINFOHEADER) + 256 * sizeof (RGBQUAD);
+    dib_hdr = (LPBITMAPINFOHEADER)malloc(dib_size);
+    memset(dib_hdr, 0, dib_size);
+    dib_hdr->biSize = sizeof(BITMAPINFOHEADER);
+    if( !GetDIBits(fl_gc, src, 0, rect.h(), NULL, (LPBITMAPINFO)dib_hdr, DIB_RGB_COLORS) )
+    {
+        free(dib_hdr);
+        //printf("GetDIBits(1) FAILED\n");
+        return 0;
+    }
 
-	src_w = dib_hdr->biWidth;
-	src_h = dib_hdr->biHeight;
+    src_w = dib_hdr->biWidth;
+    src_h = dib_hdr->biHeight;
 
-	/* clip to the drawable tree and screen */
+    /* clip to the drawable tree and screen */
     clipx = clipy = 0;
     width  = src_w - x;
     height = src_h - y;
-    
-	if(width > w) width = w;
+
+    if(width > w) width = w;
     if(height > h) height = h;
 
     if(x < 0) {
@@ -429,70 +420,71 @@ uint8 *Fl_Renderer::data_from_pixmap(Pixmap src, Fl_Rect &rect, int &bitspp)
 
     w = width;
     h = height;
-	
-	bitspp = dib_hdr->biBitCount;	
-	dib_hdr->biHeight = -h;//DIB images are upside/down...
-	dib_hdr->biWidth = w;
 
-	uint8 *lpvBits = new uint8[dib_hdr->biSizeImage];	
+    bitspp = dib_hdr->biBitCount;
+    dib_hdr->biHeight = -h;//DIB images are upside/down...
+    dib_hdr->biWidth = w;
 
-	if( !GetDIBits(fl_gc, src, 0, h, lpvBits, (LPBITMAPINFO)dib_hdr, DIB_RGB_COLORS) ) {
-		free(dib_hdr);
-		delete []lpvBits;
-		//printf("GetDIBits(2) FAILED\n");
-		return 0;
-	}
+    uint8 *lpvBits = new uint8[dib_hdr->biSizeImage];
 
-	if(x>0 || y>0) {
-		//NYI!
-	}
+    if( !GetDIBits(fl_gc, src, 0, h, lpvBits, (LPBITMAPINFO)dib_hdr, DIB_RGB_COLORS) ) {
+        free(dib_hdr);
+        delete []lpvBits;
+        //printf("GetDIBits(2) FAILED\n");
+        return 0;
+    }
 
-	return lpvBits;
+    if(x>0 || y>0) {
+        //NYI!
+    }
+
+    return lpvBits;
 }
 
 uint8 *Fl_Renderer::data_from_window(Window src, Fl_Rect &rect, int &bitspp)
 {
-	int ww=0, wh=0;
-	int px=rect.x(), py=rect.y(), pw=rect.w(), ph=rect.h();
+    Fl_Renderer::system_init();
+    int ww=0, wh=0;
+    int px=rect.x(), py=rect.y(), pw=rect.w(), ph=rect.h();
 
-	RECT R;
-	if(GetClientRect(src, &R)) {
-		ww = R.right;
-		wh = R.bottom;
-	} else
-		return 0;
-	
-	//clip...
-	if( (px+pw) > ww) {
-		pw=ww-(ww-(px+pw));
-	}
-	if( (py+ph) > wh) {
-		ph=wh-(wh-(py+ph));
-	}
-	if(pw<=0 || ph<=0)
-		return 0;
+    RECT R;
+    if(GetClientRect(src, &R)) {
+        ww = R.right;
+        wh = R.bottom;
+    } else
+        return 0;
 
-	HDC src_dc = GetDC(src);	
-	HDC dst_dc = CreateCompatibleDC(src_dc);
+    //clip...
+    if( (px+pw) > ww) {
+        pw=ww-(ww-(px+pw));
+    }
+    if( (py+ph) > wh) {
+        ph=wh-(wh-(py+ph));
+    }
+    if(pw<=0 || ph<=0)
+        return 0;
 
-	HBITMAP pixmap = CreateCompatibleBitmap(src_dc, pw, ph);
-	SelectObject(dst_dc, pixmap);
-	
-	if( !BitBlt(dst_dc, 0,0, pw, ph, src_dc, px, py, SRCCOPY) ) {
-		//printf("BitBlt() FAILED\n");		
-		return 0;
-	}
+    HDC src_dc = GetDC(src);
+    HDC dst_dc = CreateCompatibleDC(src_dc);
 
-	HDC saved = fl_gc;
-	fl_gc = dst_dc;
+    HBITMAP pixmap = CreateCompatibleBitmap(src_dc, pw, ph);
+    SelectObject(dst_dc, pixmap);
 
-	Fl_Rect r(0, 0, pw, ph);
-	uint8 *data = data_from_pixmap(pixmap, r, bitspp);
+    if( !BitBlt(dst_dc, 0,0, pw, ph, src_dc, px, py, SRCCOPY) ) {
+        //printf("BitBlt() FAILED\n");
+        return 0;
+    }
 
-	fl_gc = saved;	
-	DeleteDC(dst_dc);
+    HDC saved = fl_gc;
+    fl_gc = dst_dc;
 
-	return data;
+    Fl_Rect r(0, 0, pw, ph);
+    uint8 *data = data_from_pixmap(pixmap, r, bitspp);
+
+    fl_gc = saved;
+    DeleteDC(dst_dc);
+
+    return data;
 }
 
 Window Fl_Renderer::root_window() {
