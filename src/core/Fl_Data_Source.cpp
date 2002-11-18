6678150893a7824a6ef6e545eda1f6c26a7a24d4
Alexey Parshin <parshin@users.sf.net>
2002-11-18 00:40:05 +0000

    Mino R changes

--- src/core/Fl_Data_Source.cpp
+++ src/core/Fl_Data_Source.cpp
@@ -15,29 +15,32 @@
  *                                                                         *
  ***************************************************************************/
 
-
 #include <efltk/Fl_Data_Source.h>
 #include <efltk/Fl_Widget.h>
 #include <efltk/Fl_Group.h>
 
 bool Fl_Data_Source::load() {
    if (!parent_ || !load_data()) return false;
+/*
    for (int i = 0; i < parent_->children(); i++) {
       Fl_Widget *widget = parent_->child(i);
       if (widget->field_name()) {
          read_field(widget->field_name());
       }         
    }
-   return true; 
+*/
+   return true;
 }
 
 bool Fl_Data_Source::save() { 
    if (!parent_ || !save_data()) return false;
+/*
    for (int i = 0; i < parent_->children(); i++) {
       Fl_Widget *widget = parent_->child(i);
       if (widget->field_name()) {
          write_field(widget->field_name());
       }         
    } 
-   return true; 
+*/
+   return true;
 }
