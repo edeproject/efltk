0a5a22eeb1fce951aecb8b4d683b6127a611b8e4
Alexey Parshin <parshin@users.sf.net>
2002-11-26 05:48:49 +0000

    minor corrections

---
+++ efltk/Fl_Data_Fields.h
@@ -0,0 +1,51 @@
+/***************************************************************************
+                          Fl_Data_Fields.h  -  description
+                             -------------------
+    begin                : Mon Nov 25 2002
+    copyright            : (C) 2002 by Alexey Parshin
+    email                : alexeyp@m7.tts-sf.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef __Fl_Data_Fields_h__
+#define __Fl_Data_Fields_h__
+
+#include <efltk/Fl_Variant.h>
+#include <efltk/Fl_Ptr_List.h>
+
+class Fl_Data_Fields;
+
+class FL_API Fl_Data_Field {
+   friend class Fl_Data_Fields;
+protected:
+   Fl_String   m_name;
+public:
+   Fl_Data_Field(const char *name) { m_name = name; }
+   Fl_Variant  value;
+   const char *name() const { return m_name.c_str(); }
+};
+
+class FL_API Fl_Data_Fields {
+   Fl_Ptr_List       m_list;
+   static const Fl_Variant m_fieldNotFound;
+public:
+   Fl_Data_Fields() {}
+   ~Fl_Data_Fields() { clear(); }
+   void              clear();
+   int               count() const { return m_list.count(); }
+   int               field_index(const char * fname) const;
+   Fl_Variant&       operator [] (int index);
+   const Fl_Variant& operator [] (int index) const;
+   Fl_Variant&       operator [] (const char *fname);
+   const Fl_Variant& operator [] (const char *fname) const;
+};
+
+#endif
--- efltk/Fl_Data_Source.h
+++ efltk/Fl_Data_Source.h
@@ -18,8 +18,9 @@
 #ifndef __FL_DATA_SOURCE_H__
 #define __FL_DATA_SOURCE_H__
 
+#include <efltk/Fl_Variant.h>
+
 class Fl_Group;
-class Fl_Variant;
 
 class Fl_Data_Source {
    friend class Fl_Group;
@@ -27,18 +28,36 @@ private:
    Fl_Group* parent_;
 protected:
    // these methods should be implemented in derived class
-   virtual bool         load_data() = 0;
-   virtual bool         save_data() = 0;
+   virtual bool              load_data() = 0;
+   virtual bool              save_data() = 0;
 public:
    // ctor, dtor
    Fl_Data_Source(Fl_Group *group) { parent_ = group; }
    virtual ~Fl_Data_Source() {}
 
+   // access to the field by name
    virtual const Fl_Variant& operator [] (const char *field_name) const = 0;
    virtual Fl_Variant&       operator [] (const char *field_name) = 0;
 
-   virtual bool         read_field(const char *fname,Fl_Variant& value) = 0; 
-   virtual bool         write_field(const char *fname,const Fl_Variant& fvalue) = 0; 
+   // how many fields do we have in the current record?
+   virtual int               field_count() const = 0;
+   virtual int               field_index(const char *) const { return -1; }
+   
+   // access to the field by number, 0..field_count()-1
+   virtual const Fl_Variant& operator [] (int) const = 0;
+   virtual Fl_Variant&       operator [] (int) = 0;
+
+   virtual bool              read_field(const char *fname,Fl_Variant& value) = 0; 
+   virtual bool              write_field(const char *fname,const Fl_Variant& fvalue) = 0; 
+
+   // dataset navigation
+   virtual bool              open()  { return true; }
+   virtual bool              close() { return true; }
+   virtual bool              first() { return true; }
+   virtual bool              next()  { return true; }
+   virtual bool              prior() { return true; }
+   virtual bool              last()  { return true; }
+   virtual bool              find(Fl_Variant position) { return true; }
 
    // load data into widgets
    bool load();
