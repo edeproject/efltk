aeea566a8f7cfb0bd7d2f7fd5fd737bf0fed2e58
Alexey Parshin <parshin@users.sf.net>
2002-12-28 20:14:32 +0000

    Added new file..

---
+++ src/db/Fl_Database.cpp
@@ -0,0 +1,36 @@
+/***************************************************************************
+                          Fl_Database.cpp  -  description
+                             -------------------
+    begin                : Sat Dec 27 2002
+    copyright            : (C) 2002 by Alexey Parshin
+    email                : alexeyp@m7.tts-sf.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <db/Fl_Database.h>
+#include <db/Fl_Query.h>
+
+void Fl_Database::open(const Fl_String connString) {
+   if (connString != m_connString) {
+      close();
+      m_connString = connString;
+      open_connection();
+   }
+}
+
+void Fl_Database::close() {
+   unsigned cnt = m_queryList.count();
+   for (unsigned i = 0; i < cnt; i++) {
+      ((CQuery *)m_queryList[i])->close();
+   }
+   close_connection();
+   m_active = false;
+}
