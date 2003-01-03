1d99d0a734b3f8e77c33eba53503f333ea182de3
Mikko Lahteenmaki <laza2000@users.sf.net>
2003-01-03 23:18:46 +0000

    - Use only FL_API  - Removed old forms widget..

--- efltk/Fl_Export.h
+++ efltk/Fl_Export.h
@@ -2,24 +2,15 @@
  The following is only used when building DLLs under _WIN32
 */
 
-#if defined(_WIN32) && defined(FL_SHARED)
+#if defined(_WIN32) && defined(FL_SHARED) && (defined(_MSC_VER) || defined(__MWERKS__) || defined(__BORLANDC__) || __GNUC__ >= 3)
 # ifdef FL_LIBRARY
 #  define FL_API __declspec(dllexport)
 # else
 #  define FL_API __declspec(dllimport)
 # endif
-# ifdef FL_IMAGES_LIBRARY
-#  define FL_IMAGES_API	__declspec(dllexport)
-# else
-#  define FL_IMAGES_API	__declspec(dllimport)
-# endif
-# ifdef FL_GL_LIBRARY
-#  define FL_GL_API __declspec(dllexport)
-# else
-#  define FL_GL_API __declspec(dllimport)
-# endif
+
 #else
+
 # define FL_API
-# define FL_IMAGES_API
-# define FL_GL_API
+
 #endif
--- efltk/gl.h
+++ efltk/gl.h
@@ -54,26 +54,26 @@
 # include <GL/gl.h>
 #endif
 
-FL_GL_API void gl_start();
-FL_GL_API void gl_finish();
+FL_API void gl_start();
+FL_API void gl_finish();
 
-FL_GL_API void gl_color(Fl_Color);
+FL_API void gl_color(Fl_Color);
 
-FL_GL_API void gl_rect(int x,int y,int w,int h);
+FL_API void gl_rect(int x,int y,int w,int h);
 inline void gl_rectf(int x,int y,int w,int h) {glRecti(x,y,x+w,y+h);}
 
-FL_GL_API void gl_font(Fl_Font f, float size);
-FL_GL_API float gl_height();
-FL_GL_API float gl_descent();
-FL_GL_API float gl_width(const char *);
-FL_GL_API float gl_width(const char *, int n);
+FL_API void gl_font(Fl_Font f, float size);
+FL_API float gl_height();
+FL_API float gl_descent();
+FL_API float gl_width(const char *);
+FL_API float gl_width(const char *, int n);
 
-FL_GL_API void gl_draw(const char*);
-FL_GL_API void gl_draw(const char*, int n);
-FL_GL_API void gl_draw(const char*, float x, float y, float z = 0);
-FL_GL_API void gl_draw(const char*, int n, float x, float y, float z = 0);
+FL_API void gl_draw(const char*);
+FL_API void gl_draw(const char*, int n);
+FL_API void gl_draw(const char*, float x, float y, float z = 0);
+FL_API void gl_draw(const char*, int n, float x, float y, float z = 0);
 
-FL_GL_API void gl_draw_image(const uchar *, int x,int y,int w,int h, int d=3, int ld=0);
+FL_API void gl_draw_image(const uchar *, int x,int y,int w,int h, int d=3, int ld=0);
 
 #endif
 
