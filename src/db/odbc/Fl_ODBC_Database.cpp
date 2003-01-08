33093b36aa256a9734197b59f02d8a03249fce78
Alexey Parshin <parshin@users.sf.net>
2003-01-08 05:24:18 +0000

    Another lil step toward Fl_ODBC_database.

--- src/db/odbc/Fl_ODBC_Database.cpp
+++ src/db/odbc/Fl_ODBC_Database.cpp
@@ -19,6 +19,14 @@
 #include <efltk/db/Fl_Query.h>
 #include "fl_odbc.h"
 
+static const char
+   cantAllocateStmt[] = "Can't allocate ODBC statement";
+//   cantFreeStmt[]     = "Can't free ODBC statement";
+
+static inline BOOL successful(int ret) {
+   return ret==SQL_SUCCESS || ret==SQL_SUCCESS_WITH_INFO;
+}
+
 Fl_ODBC_Database::Fl_ODBC_Database(const Fl_String connString) 
 : Fl_Database(connString) {
    m_connect = new ODBCConnection;
@@ -64,12 +72,46 @@ void Fl_ODBC_Database::rollback_transaction() {
 }
 
 void Fl_ODBC_Database::allocate_query(Fl_Query *query) {
+   deallocate_query(query);
+   
+   void *qhandle;
+   if (SQLAllocStmt((SQLHDBC)m_connect->handle(),&qhandle) != SQL_SUCCESS) {
+      query_handle(query,SQL_NULL_HSTMT);
+      fl_throw(cantAllocateStmt);
+   }
+   query_handle(query,qhandle);
 }
 
 void Fl_ODBC_Database::deallocate_query(Fl_Query *query) {
+   void *qhandle = query_handle(query);
+   if (!qhandle) // Not allocated
+      return;
+
+   SQLFreeStmt(qhandle,SQL_DROP);
+   query_handle(query,SQL_NULL_HSTMT);
+}
+
+Fl_String Fl_ODBC_Database::query_error(Fl_Query *query) const {
+   char  errorDescription[SQL_MAX_MESSAGE_LENGTH];
+   char  errorState[SQL_MAX_MESSAGE_LENGTH];
+
+   SQLINTEGER nativeError = 0;
+   
+   SWORD  pcnmsg = 0;
+   *errorDescription = 0;
+   *errorState = 0;
+
+   if (SQL_SUCCESS!=SQLError(SQL_NULL_HENV,SQL_NULL_HDBC,query_handle(query),(UCHAR FAR*)errorState,&nativeError,(UCHAR FAR*)errorDescription,sizeof(errorDescription),&pcnmsg))
+      if (!*errorDescription) strcpy(errorDescription,"Unknown error");
+
+   return errorDescription;
 }
 
 void Fl_ODBC_Database::prepare_query(Fl_Query *query) {
+   if (!successful(SQLPrepare(query_handle(query),(UCHAR FAR *)(LPCSTR)query->sql().c_str(),SQL_NTS))) {
+      Fl_String error = query_error(query);
+      fl_throw(error);
+   }
 }
 
 void Fl_ODBC_Database::open_query(Fl_Query *query) {
