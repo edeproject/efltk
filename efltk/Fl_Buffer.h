9605b9a77daba9353f2bacf59fd658b06e17feca
Alexey Parshin <parshin@users.sf.net>
2003-02-02 17:37:37 +0000

    Added to core ..

---
+++ efltk/Fl_Buffer.h
@@ -0,0 +1,42 @@
+/***************************************************************************
+                          Fl_Buffer.h  -  description
+                             -------------------
+    begin                : Sun Feb 2 2003
+    copyright            : (C) 2000 by Alexey S.Parshin
+    email                : alexeyp@m7.tts-sf.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef __FL_BUFFER_H__
+#define __FL_BUFFER_H__
+
+#include <efltk/Fl_Export.h>
+
+class FL_API Fl_Buffer {
+protected:
+   unsigned m_size;
+   unsigned m_bytes;
+   char *   m_buffer;
+public:
+   Fl_Buffer(unsigned sz=16);
+   ~Fl_Buffer();
+   char *data() const { return m_buffer; }
+   bool checkSize(unsigned sz);
+   void set(const char *data,unsigned sz);
+   bool append(const char *data,unsigned sz);
+   void reset(unsigned sz=0);
+   void fill(char c);
+
+   unsigned size()  const { return m_size; }
+   unsigned bytes() const { return m_bytes; }
+};
+
+#endif
