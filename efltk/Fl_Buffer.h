3c035dd47ea234d1b333bc84f435f177ca6131a9
Alexey Parshin <parshin@users.sf.net>
2003-02-04 23:43:51 +0000

    Minor_update

--- efltk/Fl_Buffer.h
+++ efltk/Fl_Buffer.h
@@ -40,4 +40,10 @@ public:
    void bytes(unsigned b) { set(NULL,b); }
 };
 
+class FL_API Fl_String_Buffer {
+public:
+   Fl_String_Buffer(unsigned sz=16) : Fl_Buffer(sz) {}
+   char& operator [] (int ndx) { return mbuffer[ndx]; }
+};
+
 #endif
