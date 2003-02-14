f15b92ab0551e0015fbf85904b973b266a628da4
Dejan Lekic <leka@users.sf.net>
2003-02-14 13:58:51 +0000

    Proposed footer for eFLTK sources...

---
+++ doc/footer.c
@@ -0,0 +1,6 @@
+/*  /\ 		$id$
+ * / /\     Subproject: eFLTK
+ * \  /\ 	This source code is released under GNU LGPL License
+ *  \  / 	Copyright (c) EDE Team, 2000-today
+ *   \/ 	Equinox Desktop Environment, http://ede.sf.net
+ */
