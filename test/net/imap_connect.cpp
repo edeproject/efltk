3fa25ab9a95833ed6cecafe171a70b78214a08f0
Alexey Parshin <parshin@users.sf.net>
2003-02-15 06:08:42 +0000

    Minor corrections.

--- test/net/imap_connect.cpp
+++ test/net/imap_connect.cpp
@@ -22,7 +22,6 @@ Fl_Image *folderSmall = Fl_Image::read_xpm(0, folder_small);
 Fl_Image *fileSmall   = Fl_Image::read_xpm(0, file_small);
 
 Fl_Input *server, *user, *password;
-Fl_Choice *mailbox;
 Fl_ListView *list_view;
 Fl_ProgressBar *progress;
 Fl_Config *config;
@@ -133,9 +132,9 @@ void cb_browser(Fl_Widget *browser,void *) {
         list_view->fill(imap_ds);
         list_view->redraw();
     }
-    fl_catch(exception) {
-        puts(exception.text().c_str());
-    }
+fl_catch(exception) {
+    puts(exception.text().c_str());
+}
 }
 
 void cb_read(Fl_Widget *,void *) {
@@ -162,106 +161,94 @@ void cb_read(Fl_Widget *,void *) {
         config->write("user",user->value());
         config->write("password",password->value());
     }
-    fl_catch(exception) {
-        puts(exception.text().c_str());
-    }
+fl_catch(exception) {
+    puts(exception.text().c_str());
+}
 }
 
 Fl_Menu_Item default_mboxes[] = {
-   { "inbox", 0 },
-   { 0 }
+    { "inbox", 0 },
+    { 0 }
 };
 
 int main(int argc,char *argv[]) {
-   Fl_Window   window(600,400,"IMAP test");
-   Fl_Config   conf("imap_connect.ini");
-   Fl_String   conf_value;
-   config = &conf;
+    Fl_Window   window(600,400,"IMAP test");
+    Fl_Config   conf("imap_connect.ini");
+    Fl_String   conf_value;
+    config = &conf;
 
-   Section *sect = config->find_section("Connection");
-   if (!sect)
-      sect = config->create_section("Connection");
-   config->set_section(sect);
+    Section *sect = config->find_section("Connection");
+    if (!sect)
+        sect = config->create_section("Connection");
+    config->set_section(sect);
 
-   Fl_Group    centerGroup(10,10,150,10);
+    Fl_Group    centerGroup(10,10,150,10);
 
-   Fl_Browser  br(10,10,150,100);
-   br.layout_align(FL_ALIGN_LEFT);
-   br.indented(1);
-   br.callback(cb_browser);
-   browser = &br;
+    Fl_Browser  br(10,10,150,100);
+    br.layout_align(FL_ALIGN_LEFT);
+    br.indented(1);
+    br.callback(cb_browser);
+    browser = &br;
 
-   Fl_ListView lview(10,10,480,380);
-   lview.layout_align(FL_ALIGN_CLIENT);
-   list_view = &lview;
+    Fl_ListView lview(10,10,480,380);
+    lview.layout_align(FL_ALIGN_CLIENT);
+    list_view = &lview;
 
-   centerGroup.end();
-   centerGroup.layout_align(FL_ALIGN_CLIENT);
+    centerGroup.end();
+    centerGroup.layout_align(FL_ALIGN_CLIENT);
 
-   Fl_Group        group1(10,10,100,23);
-   group1.layout_align(FL_ALIGN_TOP);
+    Fl_Group        group1(10,10,100,23);
+    group1.layout_align(FL_ALIGN_TOP);
 
-   Fl_Box          label1(10,10,80,10,"server name");
-   label1.layout_align(FL_ALIGN_LEFT);
-   label1.align(FL_ALIGN_RIGHT|FL_ALIGN_INSIDE);
+    Fl_Box          label1(10,10,80,10,"server name");
+    label1.layout_align(FL_ALIGN_LEFT);
+    label1.align(FL_ALIGN_RIGHT|FL_ALIGN_INSIDE);
 
-   Fl_Input        input1(10,10,70,10);
-   input1.layout_align(FL_ALIGN_LEFT);
-   server = &input1;
-   conf.read("server",conf_value,"");
-   server->value(conf_value);
+    Fl_Input        input1(10,10,70,10);
+    input1.layout_align(FL_ALIGN_LEFT);
+    server = &input1;
+    conf.read("server",conf_value,"");
+    server->value(conf_value);
 
-   Fl_Box          label2(10,10,40,10,"user");
-   label2.layout_align(FL_ALIGN_LEFT);
-   label2.align(FL_ALIGN_RIGHT|FL_ALIGN_INSIDE);
+    Fl_Box          label2(10,10,40,10,"user");
+    label2.layout_align(FL_ALIGN_LEFT);
+    label2.align(FL_ALIGN_RIGHT|FL_ALIGN_INSIDE);
 
-   Fl_Input        input2(10,10,50,10);
-   input2.layout_align(FL_ALIGN_LEFT);
-   user = &input2;
-   conf.read("user",conf_value,"");
-   user->value(conf_value);
+    Fl_Input        input2(10,10,50,10);
+    input2.layout_align(FL_ALIGN_LEFT);
+    user = &input2;
+    conf.read("user",conf_value,"");
+    user->value(conf_value);
 
-	Fl_Box          label3(10,10,60,10,"password");
-	label3.layout_align(FL_ALIGN_LEFT);
-	label3.align(FL_ALIGN_RIGHT|FL_ALIGN_INSIDE);
+    Fl_Box          label3(10,10,60,10,"password");
+    label3.layout_align(FL_ALIGN_LEFT);
+    label3.align(FL_ALIGN_RIGHT|FL_ALIGN_INSIDE);
 
-	Fl_Secret_Input input3(10,10,50,10);
-	input3.layout_align(FL_ALIGN_LEFT);
-	password = &input3;
-	conf.read("password",conf_value,"");
-	password->value(conf_value);
+    Fl_Secret_Input input3(10,10,50,10);
+    input3.layout_align(FL_ALIGN_LEFT);
+    password = &input3;
+    conf.read("password",conf_value,"");
+    password->value(conf_value);
 
-	Fl_Box          label4(10,10,60,10,"mailbox");
-	label4.layout_align(FL_ALIGN_LEFT);
-	label4.align(FL_ALIGN_RIGHT|FL_ALIGN_INSIDE);
+    Fl_Button       button1(10,10,50,10,"Connect!");
+    button1.layout_align(FL_ALIGN_LEFT);
+    button1.callback(cb_read);
 
-	Fl_Choice       choice1(10,10,50,10);
-	choice1.layout_align(FL_ALIGN_LEFT);
-	choice1.menu(default_mboxes);
-	choice1.add("GGU",NULL);
-	choice1.value(0);
-	choice1.end();
-	mailbox = &choice1;
+    group1.end();
 
-	Fl_Button       button1(10,10,50,10,"Connect!");
-	button1.layout_align(FL_ALIGN_LEFT);
-	button1.callback(cb_read);
+    Fl_Group        group2(10,10,100,18);
+    group2.layout_align(FL_ALIGN_BOTTOM);
 
-	group1.end();
+    Fl_ProgressBar  pbar(10,10,100,10);
+    pbar.layout_align(FL_ALIGN_RIGHT);
+    progress = &pbar;
 
-	Fl_Group        group2(10,10,100,18);
-	group2.layout_align(FL_ALIGN_BOTTOM);
-
-	Fl_ProgressBar  pbar(10,10,100,10);
-	pbar.layout_align(FL_ALIGN_RIGHT);
-	progress = &pbar;
-
-	group2.end();
+    group2.end();
 
 
-	window.resizable(window);
-	window.end();
-	window.show();
+    window.resizable(window);
+    window.end();
+    window.show();
 
-	Fl::run();
+    Fl::run();
 }
