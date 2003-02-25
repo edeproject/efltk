43e9ee409520cd4b6573cbfd8b4b195ac8733e06
Dejan Lekic <dejan@users.sf.net>
2003-02-25 10:35:15 +0000

    Now we have decode testing also...

--- test/net/base64.cpp
+++ test/net/base64.cpp
@@ -8,16 +8,21 @@
 int main(unsigned argc, char** argv)
 {
     Fl_String strFN = argv[1];
-    Fl_Buffer bufIn;
-    Fl_String strResult;
+    Fl_Buffer bufIn, bufDecoded;
+    Fl_String strResult, strDecoded;
 	Fl_Base64 oBase64;
 
     if (strFN.length() <= 0)
-	strFN = "test.dat";
+		strFN = "test.dat";
+	/* Read binary data from given file */
     bufIn.read_file(strFN);
     printf("Size of buffer: %d\n", bufIn.bytes());
+	/* Encode data */
     oBase64.encode(strResult, bufIn);
     printf("Encoded string: %s\n", strResult.c_str());
-
+	/* Now let's try to decode encoded string */
+	oBase64.decode(bufDecoded, strResult);
+	/* And save result buffer to file */
+	bufDecoded.save_file("test2.dat");
     return 0;
 }
