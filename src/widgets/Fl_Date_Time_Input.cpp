a7e3e9a6e966d6b58d9a436c0dfd96f8a3633604
Mikko Lahteenmaki <laza2000@users.sf.net>
2003-03-27 08:19:21 +0000

    Fixed some serious desing mistakes..
     - Added styles
     - copy/set children styles in draw()
     - DO NOT use layout_align() feature in widget!!
     - Made it resizable also other way than preferred size..
     - Do not call child->resize after Fl_Widget::layout, this will cause
    layout called recursively..
    
    Why not to use layout_align() in widgets?
    Morelikely it leads to widget, which is not resizable
    in all cases. i.e. Im not using layout_align() stuff, and set my
    Fl_Date_Input height to 40 and width to 300, It must look still correct!
    It also leads to slowness, it is stupid to make simple task slower than it
    can be.. Although "so called" low-level (alex)? stuff can and IMHO should
    be in core widgets.. It's totally different case when building an
    application!
    
    Compare:
    layout() {
      do some stuff;
      Fl_Group::layout();
    }
    
    AND
    
    layout() {
      do some stuff;
      child1->resize
      child2->resize
      Fl_Widget::layout()
    }
    
    This is example of widget that has 2 children (Fl_Date_Input).
    later layout() will result much faster and convient layout..

--- src/widgets/Fl_Date_Time_Input.cpp
+++ src/widgets/Fl_Date_Time_Input.cpp
@@ -6,14 +6,7 @@
     email                : alexeyp@m7.tts-sf.com
  ***************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+// LGPL !!!
 
 #include <efltk/Fl_Date_Time_Input.h>
 #include <efltk/Fl_Masked_Input.h>
@@ -47,12 +40,6 @@ static const char * calendar_xpm[] = {
     "     .+@+@+@+@.",
     "     .........."};
 
-class Fl_Calendar_Button : public Fl_Button {
-public:
-    Fl_Calendar_Button() : Fl_Button (0,0,10,20) {}
-    void preferred_size(int& w,int& h) const { w = 20; }
-};
-
 void Fl_Date_Input::input_callback(Fl_Widget *di,void *) {
     Fl_Group *parent = (Fl_Group *) di->parent();
     if (parent)
@@ -69,41 +56,39 @@ void Fl_Date_Input::button_callback(Fl_Widget *button,void *) {
 
 static Fl_Pixmap buttonPixmap(calendar_xpm);
 
+static void date_revert(Fl_Style* s)
+{
+    s->box = FL_DOWN_BOX;
+    s->color = FL_WHITE;
+}
+
+static Fl_Named_Style date_style("Date_Input", date_revert, &Fl_Date_Input::default_style);
+Fl_Named_Style* Fl_Date_Input::default_style = &::date_style;
+
 Fl_Date_Input::Fl_Date_Input(int xx,int yy,int ww,int hh,const char *lbl)
-: Fl_Group(xx,yy,ww,hh,lbl) {
-    align(FL_ALIGN_LEFT);
-    box(FL_DOWN_BOX);
+: Fl_Group(xx,yy,ww,hh,lbl)
+{
+    style(date_style);
     layout_spacing(0);
+
     m_input = new Fl_Masked_Input(0,0,10,10);
     m_input->box(FL_FLAT_BOX);
     m_input->callback(Fl_Date_Input::input_callback);
     m_input->mask(Fl_Date_Time::dateInputFormat);
-    m_input->layout_align(FL_ALIGN_LEFT);
-    m_button = new Fl_Calendar_Button();
+    m_input->layout_align(FL_ALIGN_CLIENT);
+
+    m_button = new Fl_Button(0,0,10,10);
     m_button->box(FL_UP_BOX);
     m_button->callback(Fl_Date_Input::button_callback);
     m_button->image(&buttonPixmap);
-    m_button->layout_align(FL_ALIGN_LEFT);
+    m_button->layout_align(FL_ALIGN_RIGHT);
     end();
-}
-
-void Fl_Date_Input::prepare_layout() {
-    Fl_Boxtype bbt = m_button->box();
-    m_input->copy_style(style());
-    m_input->box(FL_FLAT_BOX);
-    m_input->button_color(FL_BLACK);
-    m_button->copy_style(style());
-    m_button->box(bbt);
-
-    // We only need to set width for m_input
-    int ww = dateInputWidth(true);
-    m_input->resize(m_input->h(),m_input->y(),ww,h());
 }
 
 int Fl_Date_Input::dateInputWidth(bool set_font) const {
     if (set_font)
         fl_font(text_font(), float(text_size()));
-    return (int) fl_width("00/00/0000 ") + 2;
+    return (int) fl_width(" 00/00/0000 ") + 2;
 }
 
 void Fl_Date_Input::preferred_size(int& w,int &h) const {
@@ -111,9 +96,29 @@ void Fl_Date_Input::preferred_size(int& w,int &h) const {
     h = int(fl_height()+fl_descent()) + box()->dh() + 2;
 }
 
-void Fl_Date_Input::layout() {
-    prepare_layout();
-    Fl_Group::layout();
+void Fl_Date_Input::draw()
+{
+    //Set style. NOTE:  this part MUST be in draw()
+    m_input->copy_style(style());
+    m_input->box(FL_FLAT_BOX);
+    m_input->button_color(FL_BLACK);
+
+    m_button->color(button_color());
+    m_button->box(button_box());
+
+    Fl_Group::draw();
+}
+
+void Fl_Date_Input::layout()
+{
+    int X=0, Y=0, W=w(), H=h();
+    box()->inset(X,Y,W,H);
+    W-=H;
+
+    m_input->resize(X, Y, W, H);
+    m_button->resize(W+box()->dx(), Y, H, H);
+
+    Fl_Widget::layout();
 }
 
 void Fl_Date_Input::value(const char *v) {
@@ -131,36 +136,66 @@ void Fl_Date_Input::date_value(Fl_Date_Time dt) {
 Fl_Date_Time Fl_Date_Input::date_value() {
     return Fl_Date_Time(m_input->value());
 }
+
 //-------------------------------------------------------------------------------
+
+static void datetime_revert(Fl_Style* s)
+{
+    s->box = FL_DOWN_BOX;
+    s->color = FL_WHITE;
+}
+
+static Fl_Named_Style datetime_style("Date_Time_Input", datetime_revert, &Fl_Date_Input::default_style);
+Fl_Named_Style* Fl_Date_Time_Input::default_style = &::datetime_style;
+
 Fl_Date_Time_Input::Fl_Date_Time_Input(int xx,int yy,int ww,int hh,const char *lbl)
-: Fl_Date_Input(xx,yy,ww,hh,lbl) {
-    begin();
-    m_timeInput = new Fl_Masked_Input(0,0,10,10," ");
-    m_timeInput->box(FL_FLAT_BOX);
+    : Fl_Date_Input(xx,yy,ww,hh,lbl)
+{
+    style(datetime_style);
+
+    Fl_Group::current(0);
+
+    m_timeInput = new Fl_Masked_Input(0,0,10,10,"");
     m_timeInput->callback(Fl_Date_Input::input_callback);
     m_timeInput->mask(Fl_Date_Time::timeInputFormat);
-    m_timeInput->label_width(16);
+    m_timeInput->label_width(10);
     m_timeInput->align(FL_ALIGN_LEFT);
     m_timeInput->layout_align(FL_ALIGN_LEFT);
+
+    // Insert to correct index, so TAB works
+    insert(*m_timeInput, 1);
+
     end();
 }
 
-void Fl_Date_Time_Input::prepare_layout() {
-    Fl_Date_Input::prepare_layout();
-
+void Fl_Date_Time_Input::draw()
+{
+    // Set style
     m_timeInput->copy_style(style());
     m_timeInput->box(FL_FLAT_BOX);
     m_timeInput->button_color(FL_BLACK);
 
-    // We only need to set width for m_input
-    int ww = timeInputWidth(true);
-    m_timeInput->resize(m_timeInput->h(),m_timeInput->y(),ww,h());
+    Fl_Date_Input::draw();
+}
+
+void Fl_Date_Time_Input::layout()
+{
+    int X=0, Y=0, W=w(), H=h();
+    box()->inset(X,Y,W,H);
+    W-=H;
+
+    m_button->resize(W+box()->dx(), Y, H, H);
+    int iw = dateInputWidth(true);
+    m_input->resize(X, Y, iw, H);
+    m_timeInput->resize(iw+X+m_timeInput->label_width(), Y, W-iw-m_timeInput->label_width(), H);
+
+    Fl_Widget::layout();
 }
 
 int Fl_Date_Time_Input::timeInputWidth(bool set_font) const {
     if (set_font)
         fl_font(text_font(), float(text_size()));
-    return (int) fl_width("00:00AM ") + 2;
+    return (int) fl_width(" 00:00 ") + 2;
 }
 
 void Fl_Date_Time_Input::preferred_size(int& w,int &h) const {
