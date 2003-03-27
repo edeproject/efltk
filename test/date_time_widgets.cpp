3b1f004ec6f05274ca0bfdaaed0dbe708b110556
Alexey Parshin <parshin@users.sf.net>
2003-03-27 16:16:06 +0000

    "Mikko, please. Think before you do something. To say it very polite, just run that test!"

--- test/date_time_widgets.cpp
+++ test/date_time_widgets.cpp
@@ -8,7 +8,7 @@ void callback(Fl_Widget *l, void *) {
 }
 
 int main() {
-    Fl_Window   window(300,200,"Date and Time widgets test");
+    Fl_Window   window(400,300,"Date and Time widgets test");
     window.layout_spacing(4);
 
     Fl_Date_Input   dateInput(0,0,10,10,"Date Input:");
@@ -18,6 +18,9 @@ int main() {
     Fl_Date_Time_Input dateTimeInput(0,0,10,10,"Date Time Input:");
     dateTimeInput.layout_align(FL_ALIGN_TOP);
     dateTimeInput.label_width(100);
+
+
+    Fl_Date_Input   dateInput2(100,100,150,150,"Date Input:");
 
     window.end();
     window.show();
