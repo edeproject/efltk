bf8db9957d688447a5699a194d5f3c82610f977f
Alexey Parshin <parshin@users.sf.net>
2003-04-02 19:58:21 +0000

    Minor corrections.

--- src/widgets/Fl_Dialog.cpp
+++ src/widgets/Fl_Dialog.cpp
@@ -299,36 +299,6 @@ static const Fl_Dialog_Button_Template buttonTemplates[] = {
     { 0,             "",       &pixmap_help }
 };
 
-// Internal DS for Fl_Dialog
-class Fl_Dialog_Data_Source : public Fl_Data_Source  {
-    friend class Fl_Dialog;
-public:
-    Fl_Dialog_Data_Source(Fl_Group *tabs) : Fl_Data_Source(tabs) {}
-
-    // access to the field value by name
-    virtual const Fl_Variant& operator [] (const char *field_name) const   { return m_fields[field_name]; }
-    virtual Fl_Variant&       operator [] (const char *field_name);
-    // access to the field by index
-    virtual const Fl_Data_Field& field (int field_index) const             { return m_fields.field(field_index); }
-    virtual Fl_Data_Field&       field (int field_index)                   { return m_fields.field(field_index); }
-    // how many rows do we have ds?
-    virtual unsigned          record_count() const { return 1; }
-    // how many fields do we have in the current record?
-    virtual unsigned          field_count() const                          { return m_fields.count(); }
-    virtual int               field_index(const char *field_name) const    { return m_fields.field_index(field_name); }
-    // access to the field by number, 0..field_count()-1
-    virtual const Fl_Variant& operator [] (int index) const                { return m_fields[index]; }
-    virtual Fl_Variant&       operator [] (int index)                      { return m_fields[index]; }
-    virtual bool              read_field(const char *fname,Fl_Variant& fvalue) { fvalue = (*this)[fname]; return true; }
-    virtual bool              write_field(const char *fname,const Fl_Variant& fvalue) { (*this)[fname] = fvalue; return true; }
-protected:
-    virtual bool              load_data() { return true; }
-    virtual bool              save_data() { return true; }
-    //Fl_Data_Field&            add(const char *fieldName) { return m_fields.add(fieldName); }
-private:
-    Fl_Data_Fields            m_fields;
-};
-
 Fl_Variant& Fl_Dialog_Data_Source::operator [] (const char *field_name) {
     int fieldIndex = field_index(field_name);
     if (fieldIndex < 0) m_fields.add(field_name);
