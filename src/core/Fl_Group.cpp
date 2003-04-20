e1d0234a821376009bb79445d711daffbd2b3669
Alexey Parshin <parshin@users.sf.net>
2003-04-20 16:38:00 +0000

    Minor updates

--- src/core/Fl_Group.cpp
+++ src/core/Fl_Group.cpp
@@ -421,7 +421,7 @@ static int widget_total_height(Fl_Widget *w,int pref_h)
 {
 	if (!(w->align() & FL_ALIGN_INSIDE)) {
 		int label_h = w->label_height();
-		if (label_h < 0) label_h = 0;		
+		if (w->label_width() < 0) label_h = 0;		
 		if (w->align() & (FL_ALIGN_LEFT|FL_ALIGN_RIGHT)) 
 			return max(label_h, pref_h);
 		return pref_h + label_h;
@@ -436,7 +436,7 @@ static void widget_position(Fl_Widget *w,int x,int y,int& wx,int& wy) {
 		wx = x;
 		wy = y;
 		if (!(w->align() & FL_ALIGN_INSIDE)) {
-			if (w->align() & FL_ALIGN_TOP)
+			if (w->align() & FL_ALIGN_TOP && label_w)
 				wy = y + w->label_height();
 		}
 	} else {
@@ -526,8 +526,9 @@ void Fl_Group::layout()
 					pref_w = o->w();
 					pref_h = hh;
 
-					if(!(o->align() & FL_ALIGN_INSIDE) && o->align()&(FL_ALIGN_TOP|FL_ALIGN_BOTTOM)) {
-						pref_h -= o->label_height();
+					if (!(o->align() & FL_ALIGN_INSIDE) && o->align()&(FL_ALIGN_TOP|FL_ALIGN_BOTTOM)) {
+						if (o->label_width() > 0)
+							pref_h -= o->label_height();
 					}
 
 					o->preferred_size(pref_w, pref_h);
@@ -552,17 +553,18 @@ void Fl_Group::layout()
 					pref_w = o->w();
 					pref_h = hh;
 
-					if(!(o->align() & FL_ALIGN_INSIDE) && o->align()&(FL_ALIGN_TOP|FL_ALIGN_BOTTOM)) {
-						pref_h -= o->label_height();
+					if (!(o->align() & FL_ALIGN_INSIDE) && o->align()&(FL_ALIGN_TOP|FL_ALIGN_BOTTOM)) {
+						if (o->label_width() > 0)
+							pref_h -= o->label_height();
 					}
 
 					o->preferred_size(pref_w, pref_h);
 
 					total_w = widget_total_width(o, pref_w);					
 
-					if(!(o->align() & FL_ALIGN_INSIDE) && o->align()&(FL_ALIGN_TOP|FL_ALIGN_BOTTOM)) {
+					if (!(o->align() & FL_ALIGN_INSIDE) && o->align()&(FL_ALIGN_TOP|FL_ALIGN_BOTTOM)) {
 						label_w = o->label_width();
-						if(label_w<0) label_w=0;
+						if (label_w<0) label_w=0;
 					}
 
 					widget_position(o, xx+ww-total_w, yy, pref_x, pref_y);
@@ -578,7 +580,7 @@ void Fl_Group::layout()
 					pref_w = ww;
 					pref_h = o->h();
 
-					if(!(o->align() & FL_ALIGN_INSIDE) && o->align()&(FL_ALIGN_RIGHT|FL_ALIGN_LEFT)) {
+					if (!(o->align() & FL_ALIGN_INSIDE) && o->align()&(FL_ALIGN_RIGHT|FL_ALIGN_LEFT)) {
 						label_w = o->label_width();
 						if (label_w < 0) label_w = 0;
 						pref_w -= label_w;
@@ -617,11 +619,6 @@ void Fl_Group::layout()
 
 					total_h = widget_total_height(o, pref_h);
 					widget_position(o, xx, yy+hh-total_h, pref_x, pref_y);
-
-					if(!(o->align() & FL_ALIGN_INSIDE) && o->align()&(FL_ALIGN_RIGHT|FL_ALIGN_LEFT)) {
-						int label_h = o->label_height();
-						if (label_h < 0) label_h = 0;
-					}
 
 					o->resize(pref_x, pref_y, pref_w, pref_h);
 
