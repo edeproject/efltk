eefb6954dfda5ea2b24cdab8527e3e93345a6d4d
Mikko Lahteenmaki <laza2000@users.sf.net>
2003-04-25 09:05:55 +0000

    Added double buffer option :) This shows how to double buffer single widget easily

--- test/curve.cpp
+++ test/curve.cpp
@@ -24,6 +24,7 @@
 //
 
 #include <efltk/Fl.h>
+#include <efltk/x.h>
 #include <efltk/Fl_Double_Window.h>
 #include <efltk/Fl_Hor_Value_Slider.h>
 #include <efltk/fl_draw.h>
@@ -37,10 +38,14 @@ const char* name[10] = {
   "X0", "Y0", "X1", "Y1", "X2", "Y2", "X3", "Y3", "rotate", "scale"
 };
 
+Fl_Slider *s[10];
+
 bool points = false;
+bool double_buffer = false;
 
 class Drawing : public Fl_Widget {
-	void draw() 
+
+	void do_drawing() 
 	{
 		int X=0, Y=0, W=w(), H=h();
     
@@ -78,11 +83,96 @@ class Drawing : public Fl_Widget {
 		fl_pop_matrix();
 		fl_pop_clip();
 	}
+
+	void draw() 
+	{
+		if(double_buffer) 
+		{
+			fl_begin_offscreen(back_buffer);
+			do_drawing();
+			fl_end_offscreen();
+			fl_copy_offscreen(x(), y(), w(), h(), back_buffer, 0, 0);
+		} else
+			do_drawing();
+	}
+
+	void layout() {
+
+		if(double_buffer) {
+			
+			if((layout_damage() & FL_LAYOUT_WH) || !back_buffer) {
+				if(back_buffer) {
+					fl_delete_offscreen(back_buffer);
+					back_buffer = 0;
+				}
+				back_buffer = fl_create_offscreen(w(), h());
+			}
+
+		} else {
+
+			if(back_buffer) {
+				fl_delete_offscreen(back_buffer);
+				back_buffer = 0;
+			}	
+
+		}
+
+		Fl_Widget::layout();
+	}
+
+	Pixmap back_buffer;
+
 public:
-	Drawing(int X,int Y,int W,int H) : Fl_Widget(X,Y,W,H) { }
+	Drawing(int X,int Y,int W,int H) : Fl_Widget(X,Y,W,H) { back_buffer = 0; }
 };
 
 Drawing *d;
+
+float s_dir = .01f, r_dir = 1;
+void anim_timeout(void *)
+{
+	args[8] += r_dir;	
+	args[9] += s_dir;	
+
+	if(args[9] >= 2) {
+		s_dir = -.01f;
+	} else if(args[9] <= 0) {
+		s_dir = .01f;
+	}
+
+	if(args[8] >= 360) {
+		r_dir = -1;
+	} else if(args[8] <= 0) {
+		r_dir = 1;
+	}
+
+	s[8]->value(args[8]);
+	s[9]->value(args[9]);
+
+	d->redraw();
+
+	Fl::repeat_timeout(0.05f, anim_timeout);
+}
+
+void anim_cb(Fl_Button *o, void*) 
+{
+	if(o->value()) {
+		Fl::add_timeout(0, anim_timeout);
+		s[8]->set_output();
+		s[9]->set_output();
+	} else {
+		Fl::remove_timeout(anim_timeout);
+		s[8]->clear_output();
+		s[9]->clear_output();
+	}
+	d->redraw();
+}
+
+void db_cb(Fl_Button *o, void*) {
+	double_buffer = o->value();
+	d->relayout();
+	d->redraw();
+}
 
 void points_cb(Fl_Button *o, void*) 
 {
@@ -98,37 +188,51 @@ void slider_cb(Fl_Slider *s, long v)
 
 int main(int argc, char** argv) 
 {
-	//Fl_Double_Window window(300,555, "Curve test");
 	Fl_Window window(300,600, "Curve test");
 	
 	Drawing drawing(10,10,280,280);
 	d = &drawing;
 
-	Fl_Group g(0, 300, 300, 300);
+	Fl_Group g(0, 300, 300, 275);
 	g.layout_spacing(2);
 
 	for (int n = 0; n<10; n++) {
-		Fl_Slider* s = new Fl_Value_Slider(name[n], 22, FL_ALIGN_TOP, 50);
-		s->align(FL_ALIGN_LEFT);
-		s->type(Fl_Slider::HORIZONTAL | Fl_Slider::TICK_ABOVE);
-		s->step(1);
-		s->value(args[n]);
-		s->callback((Fl_Callback1*)slider_cb, n);
-		s->minimum(0); s->maximum(280);
-
+		s[n] = new Fl_Value_Slider(name[n], 22, FL_ALIGN_TOP, 50);
+		s[n]->align(FL_ALIGN_LEFT);
+		s[n]->type(Fl_Slider::HORIZONTAL | Fl_Slider::TICK_ABOVE);
+		s[n]->step(1);
+		s[n]->value(args[n]);
+		s[n]->callback((Fl_Callback1*)slider_cb, n);
+		s[n]->minimum(0); 
+		s[n]->maximum(280);
+		
 		if (n == 8) {
-			s->maximum(360);
+			s[n]->maximum(360);
 		} else if (n == 9) {
-			s->maximum(2);
-			s->step(0.01);
+			s[n]->maximum(2);
+			s[n]->step(0.01);
 		}
-	}
-	Fl_Toggle_Button but("points");
-	but.callback((Fl_Callback*)points_cb);
+	}		
+
+	g.end();
+
+	Fl_Group g2(0, 575, 300, 25);
+	g2.layout_spacing(2);
+
+	Fl_Toggle_Button points_but("points", 0, FL_ALIGN_LEFT);
+	points_but.callback((Fl_Callback*)points_cb);	
+
+	Fl_Toggle_Button anim_but("animate", 0, FL_ALIGN_LEFT);		
+	anim_but.callback((Fl_Callback*)anim_cb);	
+
+	Fl_Toggle_Button db_but("double buffer", 0, FL_ALIGN_LEFT);		
+	db_but.callback((Fl_Callback*)db_cb);	
+
+	g2.end();
 
 	window.end();
 	window.resizable(d);
-	window.show(argc,argv);
+	window.show(argc,argv);	
   
 	return Fl::run();
 }
