121da31c9792f35a3b171de0a7680eac657e0656
Alexey Parshin <parshin@users.sf.net>
2003-05-16 15:57:40 +0000

    Adding event type handling to every single callback function..

--- test/arc.cpp
+++ test/arc.cpp
@@ -32,64 +32,65 @@ float args[7] = {90, 90, 100, 100, 0, 360, 0};
 const char* name[7] = {"X", "Y", "W", "H", "start", "end", "rotate"};
 
 class Drawing : public Fl_Widget {
-  void draw() {
-    fl_push_clip(0, 0, w(), h());
-    fl_color(FL_DARK3);
-    fl_rectf(0, 0, w(), h());
-    fl_push_matrix();
+    void draw() {
+        fl_push_clip(0, 0, w(), h());
+        fl_color(FL_DARK3);
+        fl_rectf(0, 0, w(), h());
+        fl_push_matrix();
     //    if (args[6]) {
-      fl_translate(w()/2.0f, h()/2.0f);
-      fl_rotate(args[6]);
-      fl_translate(-w()/2.0f, -h()/2.0f);
+        fl_translate(w()/2.0f, h()/2.0f);
+        fl_rotate(args[6]);
+        fl_translate(-w()/2.0f, -h()/2.0f);
       //}
-    fl_arc(args[0],args[1],args[2],args[3],args[4],args[5]);
-    fl_closepath();
-    fl_arc(120,120,40,40,0,-360);
-    fl_ellipse(20,20,args[2],args[3]); // hardware circle
-    fl_color(FL_WHITE);
-    fl_fill_stroke(FL_RED);
-    fl_pop_matrix();
-    fl_pop_clip();
-  }
+        fl_arc(args[0],args[1],args[2],args[3],args[4],args[5]);
+        fl_closepath();
+        fl_arc(120,120,40,40,0,-360);
+        fl_ellipse(20,20,args[2],args[3]); // hardware circle
+        fl_color(FL_WHITE);
+        fl_fill_stroke(FL_RED);
+        fl_pop_matrix();
+        fl_pop_clip();
+    }
 public:
-  Drawing(int X,int Y,int W,int H) : Fl_Widget(X,Y,W,H) {}
+    Drawing(int X,int Y,int W,int H) : Fl_Widget(X,Y,W,H) {}
 };
 
 Drawing *d;
 
 void slider_cb(Fl_Widget* o, void* v) {
-  Fl_Slider* s = (Fl_Slider*)o;
-  args[long(v)] = s->value();
-  d->redraw();
+    if (Fl::event() == FL_DATA_CHANGE) {
+        Fl_Slider* s = (Fl_Slider*)o;
+        args[long(v)] = s->value();
+        d->redraw();
+    }
 }
 
 int main(int argc, char** argv) {
-  Fl_Double_Window window(300,500);
-  Drawing drawing(10,10,280,280);
-  d = &drawing;
+    Fl_Double_Window window(300,500);
+    Drawing drawing(10,10,280,280);
+    d = &drawing;
 
-  int y = 300;
-  for (int n = 0; n<7; n++) {
-      Fl_Value_Slider* s = new Fl_Value_Slider(50,y,240,25,name[n]); y += 25;
-      s->type(Fl_Slider::HORIZONTAL);
-    if (n<4) {s->minimum(0); s->maximum(300);}
-    else if (n==6) {s->minimum(0); s->maximum(360);}
-    else {s->minimum(-360); s->maximum(360);}
-    s->type(Fl_Slider::HORIZONTAL | Fl_Slider::TICK_ABOVE);
-    s->step(1);
-    s->value(args[n]);
-    s->clear_flag(FL_ALIGN_MASK);
-    s->set_flag(FL_ALIGN_LEFT);
-    s->callback(slider_cb, (void*)n);
-  }
+    int y = 300;
+    for (int n = 0; n<7; n++) {
+        Fl_Value_Slider* s = new Fl_Value_Slider(50,y,240,25,name[n]); y += 25;
+        s->type(Fl_Slider::HORIZONTAL);
+        if (n<4) {s->minimum(0); s->maximum(300);}
+        else if (n==6) {s->minimum(0); s->maximum(360);}
+        else {s->minimum(-360); s->maximum(360);}
+        s->type(Fl_Slider::HORIZONTAL | Fl_Slider::TICK_ABOVE);
+        s->step(1);
+        s->value(args[n]);
+        s->clear_flag(FL_ALIGN_MASK);
+        s->set_flag(FL_ALIGN_LEFT);
+        s->callback(slider_cb, (void*)n);
+    }
 
-  window.end();
-  window.show(argc,argv);
-  return Fl::run();
+    window.end();
+    window.show(argc,argv);
+    return Fl::run();
 }
 
 
 //
 // End of "$Id$".
 //
-
