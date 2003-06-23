985f19701e1c35e5b98f1a9d1c29ca768f0e0f1f
Szasz Pal <space2@users.sf.net>
2003-06-23 17:17:39 +0000

    documented

--- efltk/fl_ask.h
+++ efltk/fl_ask.h
@@ -18,6 +18,9 @@
  * Please report all bugs and problems to "efltk-bugs@fltk.net"
  *
  */
+/**
+ * @file fl_ask.h
+ */
 
 #ifndef _FL_ASK_H_
 #define _FL_ASK_H_
@@ -38,25 +41,73 @@ enum BeepTypes {
     FL_BEEP_NOTIFICATION
 };
 
-/** fl_beep */
+/**
+ * Produces a short sound notification (beep)
+ * @param type is the type of the notification
+ */
 FL_API void fl_beep(int type = FL_BEEP_DEFAULT);
 
-/** fl_message */
-FL_API void fl_message(const char *,...);
+/**
+ * Shows a formatted information notice.
+ * @param msg is the message which can contain printf style formatting
+ *      characters
+ * @see fl_alert,fl_ask
+ */
+FL_API void fl_message(const char * msg,...);
 
-/** fl_alert */
-FL_API void fl_alert(const char *,...);
+/**
+ * Shows a formatted warning notice.
+ * @param msg is the message which can contain printf style formatting
+ *      characters
+ * @see fl_message,fl_ask
+ */
+FL_API void fl_alert(const char * msg,...);
 
-/** fl_ask */
-FL_API int fl_ask(const char *,...);
+/**
+ * Shows a formatted warning notice.
+ * @param msg is the message which can contain printf style formatting
+ *      characters
+ */
 
-/** fl_choice */
+/**
+ * Shows a formatted message and asks for confirmation
+ * @param msg is the message which can contain printf style formatting
+ *      characters
+ * @returns 1 if OK was pressed, otherwise 0
+ */
+FL_API int fl_ask(const char * msg,...);
+
+/**
+ * Shows a formatted message and asks what to do.
+ * The labels of the buttons can be 0.
+ * @param q is the message which can contain printf style formatting
+ *      characters
+ * @param b0 the label of the first button
+ * @param b1 the label of the second button
+ * @param b2 the label of the third button
+ * @returns the button number pressed
+ */
 FL_API int fl_choice(const char *q,const char *b0,const char *b1,const char *b2,...);
 
-/** fl_input */
+/**
+ * Shows a formatted message and waits for the user
+ * to enter a text.
+ * @param label is the message which can contain printf style formatting
+ *      characters
+ * @param deflt is the default value for the input widget
+ * @returns a pointer to the text, or NULL if the user hit cancel.
+ * @see fl_password
+ */
 FL_API const char *fl_input(const char *label, const char *deflt = 0, ...);
 
-/** fl_password */
+/**
+ * Shows a formatted message and waits for the user to enter a password
+ * @param label is the message which can contain printf style formatting
+ *      characters
+ * @param deflt is the default value for the input widget
+ * @returns a pointer to the text, or NULL if the user hit cancel.
+ * @see fl_input
+ */
 FL_API const char *fl_password(const char *label, const char *deflt = 0, ...);
 
 extern FL_API Fl_Named_Style* fl_icon_style;
