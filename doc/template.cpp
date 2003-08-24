eb25f99235827127f38bec2725198fb2b0c0a6b2
Dejan Lekic <dejan@users.sf.net>
2003-08-24 15:48:37 +0000

    Source template.

---
+++ doc/template.cpp
@@ -0,0 +1,22 @@
+/** 
+  * Foo.
+  * Some explanation about file goes here. This class is example/template
+  * class that should be used when creating new RTK classes.
+  * 
+  * The source below is under LGPL license.
+  *     Information about GNU LGPL: http://www.gnu.org/copyleft/lesser.html
+  *
+  * Copyright (c) RTK Team. More information about project: http://www.rtk.cx
+  *
+  * Authors (sorted by time they worked on this source file):
+  * 	Fname Sname, example§rtk.cx
+  *
+  * Contributors (ie. people that have sent patches, ideas, suggestions):
+  * 	Fname2 Sname2, example§rtk.cx
+  *
+  * PLEASE report any bug, bugfix, improvement or suggestion to 
+  * 	rtk-bugs§rtk.cx
+  ****************************************************************************/
+#include <rtk/foo.h>
+
+
